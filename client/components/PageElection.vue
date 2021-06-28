be71265bba46761bdb32eedb84f8c9d758558925
mende <julien.mendez@efrei.net>
2021-06-28 16:31:47 +0200

    home boutton + page élection

--- client/components/PageElection.vue
+++ client/components/PageElection.vue
@@ -3,7 +3,7 @@
   <header class="masthead bg-primary text-white text-center">
     <div class="container d-flex align-items-center flex-column">
       <!-- Masthead Heading-->
-      <h1 class="masthead-heading text-uppercase mb-0">Nom élection</h1>
+      <h1 class="masthead-heading text-uppercase mb-0">{{election.info.titre}}</h1>
       <!-- Icon Divider-->
       <div class="divider-custom divider-light">
         <div class="divider-custom-line"></div>
@@ -19,14 +19,14 @@
         <div class="divider-custom-line"></div>
       </div>
       <!-- Masthead Subheading-->
-      <p class="masthead-subheading font-weight-light mb-0"> Description en une phrase</p>
+      <p class="masthead-subheading font-weight-light mb-0"> {{election.info.dateDebut}} -  {{election.info.dateFin}} </p>
     </div>
   </header>
   <!-- Portfolio Section-->
   <section class="page-section portfolio" id="portfolio">
     <div class="container">
       <!-- Portfolio Section Heading-->
-      <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Candidat</h2>
+      <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Candidats</h2>
       <!-- Icon Divider-->
       <div class="divider-custom">
         <div class="divider-custom-line"></div>
@@ -38,65 +38,35 @@
         <div class="container">
 
           <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-            <div class="col">
-              <div class="card shadow-sm">
-                <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-                  <title>Placeholder</title>
-                  <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-                </svg>
 
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div>
-                      <a href="#">Plus d'information</a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col">
-              <div class="card shadow-sm">
-                <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-                  <title>Placeholder</title>
-                  <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-                </svg>
+                       <div class="col" v-for="candidat in election.candidat" :key="election.candidat.idChoix">
 
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div>
-                      <a href="#">Plus d'information</a>
 
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col">
-              <div class="card shadow-sm">
-                <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
-                  <title>Placeholder</title>
-                  <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-                </svg>
+                         <p>{{ candidat.libelle}}</p>
+                                        <div class="card shadow-sm">
+                                          <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                                            <title>Une chose</title>
+                                            <rect width="100%" height="100%" fill="#55595c" />
+                                          </svg>
 
-                <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div>
-                      <a href="#">Plus d'information</a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+                                          <div class="card-body">
+                                            <p class="card-text">{{candidat.description}}</p>
+                                            <div class="d-flex justify-content-between align-items-center">
+                                              <div>
+                                                <a href="#">Plus d'information</a>
+                                              </div>
+                                            </div>
+                                          </div>
+                                        </div>
 
+                                      </div>
+
+                                    </div>
+                                  </div>
+   </div>
     </div>
-  </section>
-  <!-- About Section-->
+ </section>
+ <!-- About Section-->
   <section class="page-section bg-primary text-white mb-0" id="about">
     <div class="container">
       <!-- About Section Heading-->
@@ -108,13 +78,10 @@
         <div class="divider-custom-line"></div>
       </div>
       <!-- About Section Content-->
-      <div class="row">
-        <div class="col-lg-4 ms-auto">
-          <p class="lead">Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.</p>
+      <div class="text-center mt-4">
+          <p class="lead text-center text-white">{{election.info.description}}</p>
         </div>
-        <div class="col-lg-4 me-auto">
-          <p class="lead">You can create your own custom avatar for the masthead, change the icon in the dividers, and add your email address to the contact form to make it fully functional!</p>
-        </div>
+
       </div>
       <!-- About Section Button-->
       <div class="text-center mt-4">
@@ -123,7 +90,7 @@
           Votez
         </a>
       </div>
-    </div>
+
   </section>
 </div>
 </template>
@@ -131,7 +98,7 @@
 module.exports = {
   props: {
     elections: { type: Array, default: [] },
-    election: { type: Object }
+    election: { type: Object },
   },
   data () {
     return {
@@ -140,11 +107,9 @@ module.exports = {
   async mounted () {
   },
   methods: {
-    getElections (electionsId) {
-      this.$emit('getElections')
-    },
-    getElection (electionId) {
-      this.$emit('getElection')
+
+    getElection (id) {
+      this.$emit('getElection',id)
     }
   }
 }
